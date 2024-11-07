bca24dee993aa461d264af4a4f2feb9ca2d2b6a9
MatheusHenriqueNF <matheus.henrique26@unifesp.br>
2024-11-07 19:01:14 -0300

    totem

---
+++ totem/js/script.js
@@ -0,0 +1,15 @@
+function iniciar() {
+    window.location.href = 'idioma.html'; // Substitua por sua página de destino
+}
+
+function imprimir() {
+
+    alert("Aguarde, sua rota será impressa!");
+
+  }
+
+  function ajuda() {
+
+    alert("Sua solicitação foi enviada com sucesso, aguarde!");
+
+  }
